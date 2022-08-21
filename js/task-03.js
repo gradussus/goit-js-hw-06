8d518a1ea6eb79f11be1431494d9b3d005b6de57
Vitalii Shevchenko <shevchenkovitaliy20@gmail.com>
2022-08-21 13:30:20 +0300

    На перевірку!

--- js/task-03.js
+++ js/task-03.js
@@ -1,17 +1,20 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
-const a = document.querySelector(".gallery")
-const g = images.map((image) => `<li class=gallery__item><img src=${image.url} alt=${image.alt} width=480px</li>`)
-a.insertAdjacentHTML ("afterbegin", g.join(''))
+const image = document.querySelector(".gallery");
+const g = images.map(
+  (image) =>
+    `<li class=gallery__item><img src=${image.url} alt=${image.alt} width=480px</li>`
+);
+image.insertAdjacentHTML("afterbegin", g.join(""));
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,33 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const boxes = document.querySelector("#boxes");
+const input = document.querySelector("input");
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+
+function createBoxes() {
+  const moreBoxes = [];
+  if (input.value <= 100) {
+    for (let i = 0; i < input.value; i += 1) {
+      const newBox = document.createElement("div");
+      newBox.style.backgroundColor = getRandomHexColor();
+      newBox.style.width = `${30 + i * 10}px`;
+      newBox.style.height = `${30 + i * 10}px`;
+
+      moreBoxes.push(newBox);
+
+      boxes.append(...moreBoxes);
+    }
+  } else alert(`Ну куди ж ти? Не більше 100 квадратиків)`);
+}
+function destroyBoxes() {
+  boxes.innerHTML = "";
+  input.value = "";
+}
+
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
